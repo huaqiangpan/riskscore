5b8c25c9a46dfc2e8ef5be9d38863dc525948a7f
matthewpan <matthewpan2015@hotmail.com>
2020-02-15 19:28:18 +0800

    完善了所有setup 的参数项

--- setup.py
+++ setup.py
@@ -26,22 +26,16 @@ setup(
     packages=" ",                                 #需要处理的包目录（包含__init__.py的文件夹）
     py_modules=" ",                               #需要打包的python文件列表
     download_url=" ",                             #程序的下载地址
-    cmdclass=" ",                                 #data_files
-打包时需要打包的数据文件，如图片，配置文件等
-                 - -scripts
-安装时需要执行的脚步列表
-- -package_dir
-告诉setuptools哪些目录下的文件被映射到哪个源码包。一个例子：package_dir = {'': 'lib'}，表示“root
-package”中的模块都在lib
-目录中。
---requires
-定义依赖哪些模块
-- -provides定义可以为哪些模块提供依赖
-- -find_packages()
-对于简单工程来说，手动增加packages参数很容易，刚刚我们用到了这个函数，它默认在和setup.py同一目录下搜索各个含有
-__init__.py的包。
-其实我们可以将包统一放在一个src目录中，另外，这个包内可能还有aaa.txt文件和data数据文件夹。另外，也可以排除一些特定的包
-find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
-- -install_requires = ["requests"]
-需要安装的依赖包
+    cmdclass=" ",
+    data_files=" ",                               #打包时需要打包的数据文件，如图片，配置文件等
+    scripts = " ",                                #安装时需要执行的脚步列表
+    package_dir=" ",                              #告诉setuptools哪些目录下的文件被映射到哪个源码包。一个例子：package_dir = {'': 'lib'}，表示“root package”中的模块都在lib目录中。
+    requires=" ",                                 #定义依赖哪些模块
+    provides="",                                  #定义可以为哪些模块提供依赖
+    packages = find_packages(),                   #对于简单工程来说，手动增加packages参数很容易，
+                                                  # 它默认在和setup.py同一目录下搜索各个含有__init__.py的包.其实我们可以将包统一放在一个src目录中，
+                                                  #另外，这个包内可能还有aaa.txt文件和data数据文件夹。
+                                                  #另外，也可以排除一些特定的包find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+    package_data ={'':['*.txt','*.csv'] },        #数据位于packages的子目录里
+    install_requires = ['']                       #需要安装的依赖包
 )
